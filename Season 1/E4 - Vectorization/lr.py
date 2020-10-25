f1d4e084a87398542a408181c64867a8bdbebe9d
Pavan Preetham Valluri <44934630+ppvalluri09@users.noreply.github.com>
2020-10-25 14:40:54 +0530

    Fixed the error in code

--- Season 1/E4 - Vectorization/lr.py
+++ Season 1/E4 - Vectorization/lr.py
@@ -4,7 +4,10 @@ import numpy as np
 X = np.array(list(range(1, 66)))
 y = 2*X - 1
 
-m = sum(X*y - np.mean(X)*y) / sum(X*X - np.mean(X)*X)
+# this was the line in the video, and there's a small mistake in this line
+# m = sum(X*y - np.mean(X)*y) / sum(X*X - np.mean(X)*X)
+# it is supposed to be
+m = m = sum(X*y - np.mean(y)*X) / sum(X*X - np.mean(X)*X)
 c = np.mean(y) - m * np.mean(X)
 
 print("Slope", m, ", Intercept", c)
